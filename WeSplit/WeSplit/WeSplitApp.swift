d9897de0f2c986026eb92f9d9f286b4e4b4c75b3
SUPER <superpung.lee@gmail.com>
2021-05-25 11:33:12 +0800

    feat(wesplit): init

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by SUPER on 2021/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
