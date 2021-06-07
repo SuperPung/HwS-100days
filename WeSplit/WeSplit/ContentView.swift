e9e62966ce16fe608b7d4dd087c87df2347ce573
SUPER <superpung.lee@gmail.com>
2021-06-07 13:53:39 +0800

    feat(wesplit): add total amount

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,13 +14,20 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var peopleCount: Double {
+        let count = Double(numberOfPeople + 2)
+        
+        return count
+    }
+    var grandTotal: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
-        
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    var totalPerPerson: Double {
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
@@ -50,10 +57,13 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section {
+                Section(header: Text("Total amount")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Amount per person")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
-                .padding()
             }
             .navigationBarTitle("WeSplit")
         }
