2e5135fc6dfb43e35b356e87757b1994d0e00e34
SUPER <superpung.lee@gmail.com>
2021-07-08 22:35:34 +0800

    :construction: continue myconvetion

--- MyConvention/MyConvention/ContentView.swift
+++ MyConvention/MyConvention/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State private var isMetric2English = true
     @State private var input = ""
     @State private var output = ""
+    @State private var inIndex = 0
+    @State private var outIndex = 0
     
     var inArray: [VolumeUnit] {
         return isMetric2English ? MetricUnits : EnglishUnits
@@ -18,16 +20,65 @@ struct ContentView: View {
     var outArray: [VolumeUnit] {
         return isMetric2English ? EnglishUnits : MetricUnits
     }
+    
+    var result: Double {
+        var outNum: Double = 0
+        
+        let inRate = inArray[inIndex].ratio
+        let outRate = outArray[outIndex].ratio
+        
+        let inNum = Double(input) ?? 0
+        let rate = isMetric2English ? 1/28316.8466 : 28316.8466
+        
+        outNum = inNum * inRate * inNum * rate / outRate
+        return outNum
+    }
+    
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                    TextField("from", text: $input)
-                    
+            VStack {
+                Button(action: {self.isMetric2English.toggle()}) {
+                    Text(isMetric2English ? "公制 -> 英制" : "英制 -> 公制")
+                        .font(.headline)
                 }
+                .frame(width: 120, height: 40)
+                .foregroundColor(.white)
+                .background(Color(.black))
+                .cornerRadius(CGFloat(10.0))
+                .shadow(radius: CGFloat(15.0))
                 
-                Section {
-                    Text("\(output)")
+                Form {
+                    Section (header: Text("将 \(isMetric2English ? "公制" : "英制")")
+                                .font(.subheadline).bold()
+                    ){
+                        TextField("输入\(isMetric2English ? "公制" : "英制")容积", text: $input)
+                            .frame(maxWidth: .infinity)
+                            .keyboardType(.decimalPad)
+                        
+                        Picker("", selection: $inIndex) {
+                            ForEach(0 ..< inArray.count) {index in
+                                Text(self.inArray[index].unitName)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    
+                    Section (header: Text("转换为 \(!isMetric2English ? "公制" : "英制")")
+                                .font(.subheadline).bold()
+                    ){
+                        Text("单位")
+                        Picker("", selection: $outIndex) {
+                            ForEach(0 ..< outArray.count) {index in
+                                Text(self.outArray[index].unitName)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    Section (header: Text("结果")
+                                .font(.subheadline).bold()
+                    ) {
+                        Text("\(result, specifier: "%.2f")")
+                    }
                 }
             }
             .navigationBarTitle("MyConvertion")
