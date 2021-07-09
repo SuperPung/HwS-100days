44d30757748b24ae3a4e3ee708ba1cbfa3b75525
SUPER <superpung.lee@gmail.com>
2021-07-09 11:18:24 +0800

    :tada: start project2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by SUPER on 2021/7/9.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
