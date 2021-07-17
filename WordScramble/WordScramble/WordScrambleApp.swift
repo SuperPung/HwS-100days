a596c09f669c021da004a28ca97e99167b7825b8
SUPER <superpung.lee@gmail.com>
2021-07-17 16:42:17 +0800

    :tada: start proj5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by SUPER on 2021/7/17.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
