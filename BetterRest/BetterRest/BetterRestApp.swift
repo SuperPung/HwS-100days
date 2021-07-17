fd7842369955689892ee3dadc769ce78e27b51ca
SUPER <superpung.lee@gmail.com>
2021-07-17 10:53:43 +0800

    :tada: start proj4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by SUPER on 2021/7/17.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
