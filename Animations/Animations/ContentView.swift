eee4aa2eca627eb7c2fa5034ebacf89128816c37
SUPER <superpung.lee@gmail.com>
2021-07-19 15:30:35 +0800

    :sparkles: create explicit animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        print(animationAmount)
+        
+        return VStack {
+            
+            Button("Tap me") {
+                withAnimation {
+                    self.animationAmount += 360
+                }
+            }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(
+                .degrees(animationAmount),
+                axis: (x: 0.0, y: 0.0, z: 1.0)
+            )
+            
+        }
     }
 }
 
