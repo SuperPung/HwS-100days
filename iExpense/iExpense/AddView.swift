4c58c87700455a7b7474bd3b02d4f2459e596436
SUPER <superpung.lee@gmail.com>
2021-07-20 23:34:43 +0800

    :sparkles: make changes permanent with userdefaults

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by SUPER on 2021/7/20.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    @ObservedObject var expenses: Expenses
+    
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let acturalAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: acturalAmount)
+                    self.expenses.items.append(item)
+                }
+            })
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
