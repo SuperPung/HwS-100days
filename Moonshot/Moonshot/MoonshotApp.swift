436eaa9449ffa84227d056215fda00e1052fa622
SUPER <superpung.lee@gmail.com>
2021-07-21 14:33:45 +0800

    :tada: start proj8

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by SUPER on 2021/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
