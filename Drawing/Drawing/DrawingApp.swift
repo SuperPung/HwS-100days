f84a383b1b0bc895410b67e0926c3091120b4c67
SUPER <superpung.lee@gmail.com>
2021-07-23 19:35:32 +0800

    :tada: start proj9

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by SUPER on 2021/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
