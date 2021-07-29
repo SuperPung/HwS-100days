76e2ee7c5a2a9ecb05dd1ec58bd7bd734b6c401c
SUPER <superpung.lee@gmail.com>
2021-07-29 14:31:28 +0800

    :sparkles: ensure core data objects are unique using constraints

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by SUPER on 2021/7/28.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
