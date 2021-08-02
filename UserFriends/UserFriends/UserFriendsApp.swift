17d66e73739442eaa83954d994c12df2ab7a7810
SUPER <superpung.lee@gmail.com>
2021-08-02 15:14:27 +0800

    :tada: start new challenge

---
+++ UserFriends/UserFriends/UserFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserFriendsApp.swift
+//  UserFriends
+//
+//  Created by SUPER on 2021/8/2.
+//
+
+import SwiftUI
+
+@main
+struct UserFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
