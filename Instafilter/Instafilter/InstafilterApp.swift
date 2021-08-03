98093ccd170c8cf099cd4cb240256a1e6c95459c
SUPER <superpung.lee@gmail.com>
2021-08-03 13:59:49 +0800

    :tada: start proj13

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by SUPER on 2021/8/3.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
