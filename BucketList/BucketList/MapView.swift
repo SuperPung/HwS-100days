12beb533ea3892fc2626023a347c50913d382886
SUPER <superpung.lee@gmail.com>
2021-08-07 15:31:02 +0800

    :sparkles: use MapKit

---
+++ BucketList/BucketList/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  BucketList
+//
+//  Created by SUPER on 2021/8/7.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
