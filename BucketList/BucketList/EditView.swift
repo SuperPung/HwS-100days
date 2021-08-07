cc73b4272517f5b1572b96f2f712e47db2595ecf
SUPER <superpung.lee@gmail.com>
2021-08-08 00:54:04 +0800

    :sparkles: try add wikipedia

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -11,6 +11,12 @@ import MapKit
 struct EditView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var placemark: MKPointAnnotation
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
     
     var body: some View {
         NavigationView {
@@ -18,6 +24,22 @@ struct EditView: View {
                 Section {
                     TextField("Place name", text: $placemark.wrappedTitle)
                     TextField("Description", text: $placemark.wrappedSubtitle)
+                }
+                
+                Section {
+                    if loadingState == .loaded {
+                        List(pages, id:\.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    } else if loadingState == .loading {
+                        Text("Loading...")
+                    } else {
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationBarTitle("Edit place")
