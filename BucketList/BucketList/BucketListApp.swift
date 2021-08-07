28fe86261fcd064698e0bdc767f6ed46a222fdaf
SUPER <superpung.lee@gmail.com>
2021-08-07 15:05:54 +0800

    :tada: start proj14

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by SUPER on 2021/8/7.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
