8bfdc84ef0369a34c044be3262343de09ba8a512
SUPER <superpung.lee@gmail.com>
2021-08-16 21:02:09 +0800

    :tada: start proj16

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by SUPER on 2021/8/16.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
