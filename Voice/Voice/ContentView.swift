776c0c30b908d65a3b8677d35c0f5544a2400f92
SUPER <superpung.lee@gmail.com>
2021-08-16 13:38:16 +0800

    :tada: start proj15

---
+++ Voice/Voice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Voice
+//
+//  Created by SUPER on 2021/8/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Voice/Voice/VoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceApp.swift
+//  Voice
+//
+//  Created by SUPER on 2021/8/16.
+//
+
+import SwiftUI
+
+@main
+struct VoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
