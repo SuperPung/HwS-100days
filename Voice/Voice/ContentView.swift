50ec72c5467e32306b57c34a5c83f371fba67841
SUPER <superpung.lee@gmail.com>
2021-08-16 14:54:31 +0800

    :construction: finish proj15

--- Voice/Voice/ContentView.swift
+++ Voice/Voice/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "郁金香",
+        "冷冻树芽",
+        "向日葵",
+        "焰火"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Image(pictures[selectedPicture])
+//            Image(decorative: "character")
+//            .resizable()
+//            .scaledToFit()
+//            .onTapGesture {
+//                self.selectedPicture = Int.random(in: 0...3)
+//            }
+//            .accessibility(label: Text(labels[selectedPicture]))
+//            .accessibility(addTraits: .isButton)
+//            .accessibility(removeTraits: .isImage)
+//            .environment(\.locale, Locale(identifier: "zh_CN"))
+        VStack {
+            Text("你的分数是")
+            Text("1000")
+                .font(.title)
+        }
+        .accessibilityElement(children: .combine)
+        .environment(\.locale, Locale(identifier: "zh_CN"))
     }
 }
 
