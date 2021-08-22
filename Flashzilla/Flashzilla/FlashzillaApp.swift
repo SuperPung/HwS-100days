21feec48e987908e787567388e289fd786aabaae
SUPER <superpung.lee@gmail.com>
2021-08-22 17:11:46 +0800

    :tada: start proj17

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by SUPER on 2021/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
