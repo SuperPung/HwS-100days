960dd60e1e213099daa6619f403ec13dbb32c4ec
SUPER <superpung.lee@gmail.com>
2021-09-06 12:21:34 +0800

    :sparkles: end the app with allowsHitTesting()

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -50,6 +50,15 @@ struct ContentView: View {
                         .stacked(at: index, in: self.cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
+                }
             }
             
             if differentiateWithoutColor {
@@ -77,7 +86,9 @@ struct ContentView: View {
             self.isActive = false
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            self.isActive = true
+            if self.cards.isEmpty == false {
+                self.isActive = true
+            }
         }
         .onReceive(timer) { time in
             guard self.isActive else { return }
@@ -89,6 +100,15 @@ struct ContentView: View {
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = [Card](repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
