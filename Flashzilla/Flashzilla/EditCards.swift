706fc450bb74da7605b5213fa907686fdcfa6b40
SUPER <superpung.lee@gmail.com>
2021-09-06 22:07:53 +0800

    :art: update card

---
+++ Flashzilla/Flashzilla/EditCards.swift
@@ -0,0 +1,20 @@
+//
+//  EditCards.swift
+//  Flashzilla
+//
+//  Created by SUPER on 2021/9/6.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditCards_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCards()
+    }
+}
