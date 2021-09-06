173f3f256dbf9afc61898fdc178a929c3336622f
SUPER <superpung.lee@gmail.com>
2021-09-06 14:27:34 +0800

    :sparkles: make iPhones vibrate with UINotificationFeedbackGenerator

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -13,6 +13,7 @@ struct CardView: View {
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     var removal: (() -> Void)? = nil
+    @State private var feedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
@@ -53,10 +54,17 @@ struct CardView: View {
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
+                    self.feedback.prepare()
                 }
             
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
+                        
                         self.removal?()
                     } else {
                         self.offset = .zero
