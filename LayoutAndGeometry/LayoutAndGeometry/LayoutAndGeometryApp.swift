2b5defb19c78779d42ed2a314a32db18b3496a98
SUPER <superpung.lee@gmail.com>
2021-09-07 14:42:08 +0800

    :tada: start proj18

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by SUPER on 2021/9/7.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
