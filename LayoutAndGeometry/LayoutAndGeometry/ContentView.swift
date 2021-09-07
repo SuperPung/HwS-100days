eac01e00628da21293b82d6843999d84ac3a1b7d
SUPER <superpung.lee@gmail.com>
2021-09-07 16:03:42 +0800

    :art: use geometry reader in scroll view

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -18,21 +18,21 @@ extension VerticalAlignment {
 }
 
 struct ContentView: View {
-    var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("paul-hudson")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5), axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
             }
         }
     }
