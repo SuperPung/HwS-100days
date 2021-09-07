75bb349243f36e749a6cecad79195661a8903ea8
SUPER <superpung.lee@gmail.com>
2021-09-08 00:17:27 +0800

    :bug: fix display bug

---
+++ SnowSeeker/SnowSeeker/Facility.swift
@@ -0,0 +1,8 @@
+//
+//  Facility.swift
+//  SnowSeeker
+//
+//  Created by SUPER on 2021/9/8.
+//
+
+import Foundation
---
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -0,0 +1,58 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by SUPER on 2021/9/7.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Group {
+                    HStack {
+                        if sizeClass == .compact {
+                            Spacer()
+                            VStack { ResortDetailsView(resort: resort) }
+                            VStack { SkiDetailsView(resort: resort) }
+                            Spacer()
+                        } else {
+                            ResortDetailsView(resort: resort)
+                            Spacer().frame(height: 0)
+                            SkiDetailsView(resort: resort)
+                        }
+                    }
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .padding(.top)
+                    
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(ListFormatter.localizedString(byJoining: resort.facilities))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationBarTitle(Text("\(resort.name), \(resort.country)"), displayMode: .inline)
+    }
+}
+
+struct ResortView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortView(resort: Resort.example)
+    }
+}
